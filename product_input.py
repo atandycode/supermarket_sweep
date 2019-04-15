abd6b2a5b74458e580b7797618b0c5c124c9b467
Andrew Tandy <andrewtandy2@gmail.com>
2019-04-15 14:44:20 +0100

    building python classes

---
+++ product_input.py
@@ -0,0 +1,12 @@
+class ProductInput:
+    def __init__(self):
+        self.product = input("Please enter product: ")
+
+    def product_input(self):
+        return self.product
+
+    def print_product(self):
+        print(f"You are searching for {self.product}, is that correct?")
+
+test = ProductInput()
+test.product_input()
